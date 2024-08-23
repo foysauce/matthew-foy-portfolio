d8d9ce3a3748321316b392929c7d38c69f4e2401
foysauce <mr.foy488@gmail.com>
2024-08-23 06:37:42 -0400

    added sentry.io

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,14 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <h1>
-          <FloatingNav
-            navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-          />
+          <FloatingNav navItems={navItems} />
           <Hero />
           <Grid />
           <RecentProject />
